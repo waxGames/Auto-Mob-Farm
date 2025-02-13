75f82642cb4600c2c13c6057dc2ca67a67d2f734
Silver Team <145873582+waxGames@users.noreply.github.com>
2025-02-13 23:47:51 +0300

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,29 @@
+import threading
+import pyautogui
+import keyboard
+import time
+import os
+
+print("Bu program @Flix tarafindan geliştirilmiştir, iyi kullanimlar.\nÇikmak için 'O'-'o' tuşuna basa bilirsiniz.")
+screen_width, screen_height = pyautogui.size()
+center_x = screen_width // 2
+center_y = screen_height // 2
+
+def loop():
+    pyautogui.moveTo(x=center_x, y=center_y)
+    pyautogui.press("w")
+    time.sleep(1)
+    pyautogui.click()
+    pyautogui.press("s")
+
+def q_pressed():
+    while True:
+        if keyboard.is_pressed("o"):
+            print("Program kapatılıyor...")
+            os._exit(0) 
+
+thread_q = threading.Thread(target=q_pressed, daemon=True)
+thread_q.start()
+
+while True:
+    loop()
