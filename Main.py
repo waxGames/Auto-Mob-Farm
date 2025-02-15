661a1980dc2cdfc3e6e8fd91d8ab9e3fd76b4588
Silver Team <145873582+waxGames@users.noreply.github.com>
2025-02-15 10:13:16 +0300

    Update Langs Main.py

--- Main.py
+++ Main.py
@@ -2,9 +2,31 @@ import threading
 import pyautogui
 import keyboard
 import time
+import json
 import os
 
-print("Bu program @SilverTeam tarafindan geliştirilmiştir, iyi kullanimlar.\nÇikmak için 'O'-'o' tuşuna basa bilirsiniz.")
+lang = ""
+
+def lang_select():
+    global lang
+    lang_selec = input("Pls select langs[tr, en, ru]: ")
+    lang_ = lang_selec.lower()
+    if lang_ == "ru":
+        lang = "ru.json"
+    elif lang_ == "en":
+        lang = "en.json"
+    elif lang_ == "tr":
+        lang = "tr.json"
+    else:
+        print("Please select valid language.")
+        return lang_select()
+    
+lang_select()
+
+with open(f"langs/{lang}", "r", encoding="utf-8") as file:
+    lang_data = json.load(file)
+
+print(lang_data["creator_message"])
 screen_width, screen_height = pyautogui.size()
 center_x = screen_width // 2
 center_y = screen_height // 2
@@ -19,7 +41,7 @@ def loop():
 def q_pressed():
     while True:
         if keyboard.is_pressed("o"):
-            print("Program kapatılıyor...")
+            print(lang_data["progam"])
             os._exit(0) 
 
 thread_q = threading.Thread(target=q_pressed, daemon=True)
